345518a7ef70e74e1609aeaeeec1e4d969ce843e
sruthisami <sheisruthisami@gmail.com>
2026-09-13 20:25:07 +0530

    rebase

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -66,10 +66,11 @@ const FACULTY_COORDINATORS = [
 ]
 
 const EVENT_COORDINATORS = [
+  { name: "Srikar Burgula", role: ["Events Coordinator"], phone: "+91 8328292124", photoUrl: "/coordinators/srikar.jpeg" },
   { name: "P Maheshwar", role: ["Web & IT Coordinator"], phone: "+91 9515871625", photoUrl: "/coordinators/mahesh.jpeg" },
   { name: "E. V. Gaurav", role: ["Registrations & Help Desk Coordinator"], phone: "+91 8179590621", photoUrl: "/coordinators/gaurav.jpeg" },
-  { name: "Sarvani Divakarla", role: ["Web & IT Coordinator"], phone: "+91 8179590621", photoUrl: "/coordinators/sarvani divakarla.jpeg" },
-  { name: "Naga Sresht", role: ["Web & IT Coordinator"], phone: "+91 8179590621", photoUrl: "/coordinators/naga sresht.jpeg" },
+  { name: "Sarvani Divakarla", role: ["Web & IT Coordinator"], phone: "+91 6302944144", photoUrl: "/coordinators/sarvani%20divakarla.jpeg" },
+  { name: "Naga Sresht", role: ["Web & IT Coordinator"], phone: "+91 8978531856", photoUrl: "/coordinators/naga%20sresht.jpeg" },
 ]
 
 const DEVELOPERS = [
