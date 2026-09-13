d1d6eb3e4359bfbe16114301a2e3a8667121e63e
sruthisami <sheisruthisami@gmail.com>
2026-09-13 23:37:32 +0530

    resolve next image cache

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -68,7 +68,7 @@ const FACULTY_COORDINATORS = [
 const EVENT_COORDINATORS = [
   { name: "P Maheshwar", role: ["Web & IT Coordinator"], phone: "+91 9515871625", photoUrl: "/coordinators/mahesh.jpeg" },
   { name: "E. V. Gaurav", role: ["Registrations & Help Desk Coordinator"], phone: "+91 8179590621", photoUrl: "/coordinators/gaurav.jpeg" },
-  { name: "Sarvani Divakarla", role: ["Web & IT Coordinator"], phone: "+91 6302944144", photoUrl: "/coordinators/sarvani_divakarla_updated.jpeg" },
+  { name: "Sarvani Divakarla", role: ["Web & IT Coordinator"], phone: "+91 6302944144", photoUrl: "/coordinators/sarvani_divakarla.jpeg" },
   { name: "Naga Sresht", role: ["Web & IT Coordinator"], phone: "+91 8978531856", photoUrl: "/coordinators/naga%20sresht.jpeg" },
 ]
 
